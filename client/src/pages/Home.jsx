1b4fcc872724d3ee19fa8b774ea6ee906fb79a4c
taikool2121996 <taikool2121996@gmail.com>
2023-04-23 13:11:07 +0700

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from '../config/motion';
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                LET'S <br className="xl:block hidden" />
+                <span className="rainbow_gradient ">MAKE IT</span>
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Utilize brand-new 3D customizing tool to make your special and unique
+                T-shirt. <strong>Imagination at full throttle</strong>{' '}and define your personal style.
+              </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize Your Own"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
