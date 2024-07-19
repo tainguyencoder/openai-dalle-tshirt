8f6641e92cfd6e337d960a7bbd4d2cc1af152b31
tainguyencoder <tainguyencoder@gmail.com>
2024-07-19 10:50:09 +0700

    migrate v3 to v4 openai

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   development: {
-    // backendUrl: "http://localhost:8080/api/v1/dalle",
-    backendUrl: "https://openai-dalle-tshirt.onrender.com/api/v1/dalle",
+    backendUrl: "http://localhost:8080/api/v1/dalle",
+    // backendUrl: "https://openai-dalle-tshirt.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://openai-dalle-tshirt.onrender.com/api/v1/dalle",
--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,15 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import { Configuration, OpenAIApi} from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
-const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" })
@@ -20,7 +19,7 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
