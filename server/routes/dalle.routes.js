4b24232a0445399d57402fe13553cddb02ec4b7c
tainguyencoder <tainguyencoder@gmail.com>
2024-07-19 11:19:36 +0700

    add error catching again

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,13 +7,12 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
 
-
 router.route('/').get((req, res) => {
-  res.status(200).json({ message: "Hello from DALL.E ROUTES" })
-})
+  res.status(200).json({ message: 'Hello from DALL.E ROUTES' });
+});
 
 router.route('/').post(async (req, res) => {
   try {
@@ -23,7 +22,7 @@ router.route('/').post(async (req, res) => {
       prompt,
       n: 1,
       size: '1024x1024',
-      response_format: 'b64_json'
+      response_format: 'b64_json',
     });
 
     const image = response.data.data[0].b64_json;
@@ -31,14 +30,15 @@ router.route('/').post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     if (error instanceof OpenAI.APIError) {
-      console.error(error.status);  // e.g. 401
+      console.error(error.status); // e.g. 401
       console.error(error.message); // e.g. The authentication token you passed was invalid...
-      console.error(error.code);  // e.g. 'invalid_api_key'
-      console.error(error.type);  // e.g. 'invalid_request_error'
+      console.error(error.code); // e.g. 'invalid_api_key'
+      console.error(error.type); // e.g. 'invalid_request_error'
     } else {
       // Non-API error
       console.log(error);
     }
-})
+  }
+});
 
 export default router;
